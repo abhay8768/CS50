2f94d6e7bb9887eb8e9d97043b9a4e5497e6f239
weixiaowei <1575144082@qq.com>
2014-09-08 14:44:35 +0800

    Create copyof.c
    the difference of copy , the '\0', pointer, x[n]

---
+++ copyof.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+
+int main(void)
+{
+   
+    
+/*   char* s = "abcdefghijkmn";*/
+/*   char c[3];*/
+/*   */
+/*   */
+/*   c[0] = s[0];*/
+/*   c[1] = s[1];*/
+/*   c[2] = s[2];*/
+/*   */
+/*   */
+/*   printf("cur is %s\n", c);*/
+
+     char s[3];
+     s[0] = 'e';
+     s[1] = 'e';
+     s[2] = 'u';
+     printf("%s", s);
+    
+    
+}
